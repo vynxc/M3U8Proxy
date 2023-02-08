ae01fe1714f5431a522461c114798756b1c20e3f
vynxcc <svynxc@gmail.com>
2023-02-07 21:45:30 -0500

    added rel support on m3u8 route

--- M3U8Proxy/Controllers/Proxy.cs
+++ M3U8Proxy/Controllers/Proxy.cs
@@ -16,6 +16,203 @@ namespace M3U8Proxy.Controllers;
 public class Proxy : Controller
 
 {
+    [HttpGet("{url}/{headers?}")]
+    public Task GetProxy(string url, string? headers = "{}")
+    {
+        try
+        {
+            //decode url and headers
+            url = HttpUtility.UrlDecode(url);
+            headers = HttpUtility.UrlDecode(headers);
+
+            //convert headers to dictionary
+            var headersDictionary = JsonConvert.DeserializeObject<Dictionary<string, string>>(headers);
+
+            var options = HttpProxyOptionsBuilder.Instance
+                //disable forwarded headers
+                .WithShouldAddForwardedHeaders(false)
+
+                //remove cors blocked headers
+                .WithBeforeSend((c, hrm) =>
+                {
+                    foreach (var header in _corsBlockedHeaders)
+                    {
+                        var headerToRemove =
+                            hrm.Headers.FirstOrDefault(h =>
+                                h.Key.Equals(header, StringComparison.InvariantCultureIgnoreCase)).Key;
+
+                        if (headerToRemove != null) hrm.Headers.Remove(headerToRemove);
+                    }
+
+                    if (headersDictionary != null)
+                        foreach (var header in headersDictionary)
+                        {
+                            var headerToRemove =
+                                hrm.Headers.FirstOrDefault(h =>
+                                    h.Key.Equals(header.Key, StringComparison.InvariantCultureIgnoreCase)).Key;
+                            if (headerToRemove != null) hrm.Headers.Remove(headerToRemove);
+                            hrm.Headers.Add(header.Key, header.Value);
+                        }
+
+                    return Task.CompletedTask;
+                })
+                //handle errors
+                .WithHandleFailure(async (context, e) =>
+                {
+                    context.Response.StatusCode = context.Response.StatusCode;
+                    await context.Response.WriteAsync(e.Message);
+                })
+                //remove cors blocked headers
+                .WithAfterReceive((c, hrm) =>
+                {
+                    foreach (var header in _corsBlockedHeaders) hrm.Headers.Remove(header.ToLower());
+
+                    return Task.CompletedTask;
+                })
+                .Build();
+            //return proxy
+            return this.HttpProxyAsync(url, options);
+        }
+        catch (Exception e)
+        {
+            //handle errors
+            HttpContext.Response.StatusCode = 400;
+            HttpContext.Response.ContentType = "application/json";
+            HttpContext.Response.WriteAsync(JsonConvert.SerializeObject(e.Message));
+            return Task.FromResult(0);
+        }
+    }
+
+    [HttpGet("m3u8/{url}/{headers?}")]
+    public async Task<IActionResult> GetM3U8(string url, string? headers = "{}")
+    {
+        var hasKeyword = false;
+        var listOfKeywords = new List<string> { "#EXT-X-STREAM-INF", "#EXT-X-I-FRAME-STREAM-INF" };
+        var baseUrl = "https://proxy.vnxservers.com/";
+        var proxyUrl = baseUrl + "proxy/";
+        var m3U8Url = baseUrl + "proxy/m3u8/";
+
+        //check if url is empty
+        if (string.IsNullOrEmpty(url))
+            return BadRequest("URL missing or malformed.");
+
+        try
+        {
+            //decode url
+            url = HttpUtility.UrlDecode(url);
+
+            //decode headers
+            headers = HttpUtility.UrlDecode(headers)!;
+
+            //deserialize headers
+            var headersDictionary = JsonConvert.DeserializeObject<Dictionary<string, string>>(headers);
+
+            //make request
+            var response = MakeRequest(url, headersDictionary!);
+
+            RemoveBlockedHeaders(response);
+            AddResponseHeaders(response);
+
+            //validate response
+            var content = await FixUrlsInM3U8File(response, url);
+            if (listOfKeywords.Any(keyword => content.Contains(keyword)))
+                hasKeyword = true;
+            var modifiedContent = ModifyContent(content, hasKeyword ? m3U8Url : proxyUrl, headers);
+
+            //return file
+            return File(Encoding.UTF8.GetBytes(modifiedContent), "application/vnd.apple.mpegurl",
+                $"{url.Substring(0, 15)}.m3u8");
+        }
+        catch (Exception ex)
+        {
+            //return error
+            return BadRequest(ex.Message);
+        }
+    }
+
+    #region PrivateMethods
+
+    private async Task<string> FixUrlsInM3U8File(IRestResponse response, string url)
+    {
+        //create url from string
+        var m3U8Uri = new Uri(url);
+
+        //get filename from url
+        var fileName = Path.GetFileName(m3U8Uri.AbsolutePath);
+
+        //get base url
+        var baseUrl =
+            $"{m3U8Uri.GetLeftPart(UriPartial.Authority)}{m3U8Uri.AbsolutePath.Substring(0, m3U8Uri.AbsolutePath.Length - fileName.Length)}"
+            !;
+
+        //split response content into lines
+        var lines = response.Content.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+
+        //loop through lines
+        for (var i = 0; i < lines.Length; i++)
+            //if line does not start with http, # or is empty
+            if (!lines[i].StartsWith("http") && !lines[i].StartsWith("#") && !string.IsNullOrWhiteSpace(lines[i]))
+            {
+                //if line does not start with /
+                if (lines[i].StartsWith("/"))
+                    lines[i] = lines[i][..1];
+
+                //if base url does not end with /
+                if (baseUrl!.EndsWith("/"))
+                    baseUrl = baseUrl.Append('/').ToString();
+
+
+                //create absolute url
+                var absoluteUrl = baseUrl + lines[i];
+
+                //replace line with absolute url
+                lines[i] = absoluteUrl;
+            }
+
+        //combine lines into string
+        return string.Join(Environment.NewLine, lines);
+    }
+
+    private void RemoveBlockedHeaders(IRestResponse response)
+    {
+        foreach (var header in _corsBlockedHeaders)
+        {
+            var headerToRemove =
+                response.Headers.FirstOrDefault(h =>
+                    h.Name.Equals(header, StringComparison.InvariantCultureIgnoreCase));
+            if (headerToRemove != null) response.Headers.Remove(headerToRemove);
+        }
+    }
+
+    private string ModifyContent(string content, string prefix, string headers)
+    {
+        var headersEncoded = HttpUtility.UrlEncode(headers);
+        //replace urls with proxy urls
+        return Regex.Replace(content, @"https?:\/\/[^\s""]+",
+            match => { return prefix + HttpUtility.UrlEncode(match.Value) + "/" + headersEncoded; });
+    }
+
+    private IRestResponse MakeRequest(string url, Dictionary<string, string> headersDictionary)
+    {
+        var client = new RestClient(url) { Timeout = -1 };
+        var request = new RestRequest(Method.GET);
+
+        foreach (var header in headersDictionary) request.AddHeader(header.Key, header.Value);
+        return client.Execute(request);
+    }
+
+    private void AddResponseHeaders(IRestResponse response)
+    {
+        //remove cors headers
+        foreach (var header in response.Headers.Where(h =>
+                     _corsBlockedHeaders.Contains(h.Name, StringComparer.InvariantCultureIgnoreCase)))
+            response.Headers.Remove(header);
+        //add headers to response
+        foreach (var header in response.Headers.Where(h =>
+                     h.Type == ParameterType.HttpHeader && h.Name != "Transfer-Encoding"))
+            HttpContext.Response.Headers.Add(header.Name, (string)header.Value);
+    }
+
     private readonly string[] _corsBlockedHeaders =
     {
         "Access-Control-Allow-Origin",
@@ -33,139 +230,6 @@ public class Proxy : Controller
         "x-amz-cf-pop",
         "x-amz-cf-id"
     };
-
-    [HttpGet("{url}/{headers?}")]
-    public Task GetProxy(string url, string? headers = "{}")
-    {
-        try
-        {
-            url = HttpUtility.UrlDecode(url);
-            headers = HttpUtility.UrlDecode(headers);
-            var headersDictionary = JsonConvert.DeserializeObject<Dictionary<string, string>>(headers);
-
-            var options = HttpProxyOptionsBuilder.Instance
-                .WithShouldAddForwardedHeaders(false)
-                .WithBeforeSend((c, hrm) =>
-                {
-                    foreach (var header in _corsBlockedHeaders)
-                    {
-                        var headerToRemove =
-                            hrm.Headers.FirstOrDefault(h =>
-                                h.Key.Equals(header, StringComparison.InvariantCultureIgnoreCase)).Key;
-                        if (headerToRemove != null) hrm.Headers.Remove(headerToRemove);
-                    }
-
-                    if (headersDictionary != null)
-                        foreach (var header in headersDictionary)
-                        {
-                            var headerToRemove =
-                                hrm.Headers.FirstOrDefault(h =>
-                                    h.Key.Equals(header.Key, StringComparison.InvariantCultureIgnoreCase)).Key;
-                            if (headerToRemove != null) hrm.Headers.Remove(headerToRemove);
-                            hrm.Headers.Add(header.Key, header.Value);
-                        }
-
-                    return Task.CompletedTask;
-                })
-                .WithHandleFailure(async (context, e) =>
-                {
-                    context.Response.StatusCode = context.Response.StatusCode;
-                    await context.Response.WriteAsync(e.Message);
-                })
-                .WithAfterReceive((c, hrm) =>
-                {
-                    foreach (var header in _corsBlockedHeaders) hrm.Headers.Remove(header.ToLower());
-
-                    return Task.CompletedTask;
-                })
-                .Build();
-
-            return this.HttpProxyAsync(url, options);
-        }
-        catch (Exception e)
-        {
-            HttpContext.Response.StatusCode = 400;
-            HttpContext.Response.ContentType = "application/json";
-            HttpContext.Response.WriteAsync(JsonConvert.SerializeObject(e.Message));
-            return Task.FromResult(0);
-        }
-    }
-
-    [HttpGet("m3u8/{url}/{headers?}")]
-    public async Task<IActionResult> GetM3U8(string url, string? headers = "{}")
-    {
-        var hasKeyword = false;
-        var listOfKeywords = new List<string> { "#EXT-X-STREAM-INF", "#EXT-X-I-FRAME-STREAM-INF" };
-        var baseUrl = "https://proxy.vnxservers.com/";
-        var proxyUrl = baseUrl + "proxy/";
-        var m3u8Url = baseUrl + "proxy/m3u8/";
-
-        if (string.IsNullOrEmpty(url))
-            return BadRequest("URL missing or malformed.");
-
-        try
-        {
-            url = HttpUtility.UrlDecode(url);
-            headers = HttpUtility.UrlDecode(headers);
-            var headersDictionary = JsonConvert.DeserializeObject<Dictionary<string, string>>(headers);
-            var response = MakeRequest(url, headersDictionary!);
-
-            RemoveBlockedHeaders(response);
-            AddResponseHeaders(response);
-
-            if (listOfKeywords.Any(keyword => response.Content.Contains(keyword)))
-                hasKeyword = true;
-            var modifiedContent = ModifyContent(response.Content, hasKeyword ? m3u8Url : proxyUrl, headers);
-            return File(Encoding.UTF8.GetBytes(modifiedContent), "application/vnd.apple.mpegurl",
-                $"{url.Substring(0, 15)}.m3u8");
-        }
-        catch (Exception ex)
-        {
-            return BadRequest(ex.Message);
-        }
-    }
-
-    #region Private Methods
-
-    private void RemoveBlockedHeaders(IRestResponse response)
-    {
-        foreach (var header in _corsBlockedHeaders)
-        {
-            var headerToRemove =
-                response.Headers.FirstOrDefault(h =>
-                    h.Name.Equals(header, StringComparison.InvariantCultureIgnoreCase));
-            if (headerToRemove != null) response.Headers.Remove(headerToRemove);
-        }
-    }
-
-    private string ModifyContent(string content, string prefix, string headers)
-    {
-        var headersEncoded = HttpUtility.UrlEncode(headers);
-        return Regex.Replace(content, @"https?:\/\/[^\s""]+",
-            match => { return prefix + HttpUtility.UrlEncode(match.Value) + "/" + headersEncoded; });
-    }
-
-
-    private IRestResponse MakeRequest(string url, Dictionary<string, string> headersDictionary)
-    {
-        var client = new RestClient(url) { Timeout = -1 };
-        var request = new RestRequest(Method.GET);
-
-        foreach (var header in headersDictionary) request.AddHeader(header.Key, header.Value);
-        return client.Execute(request);
-    }
-
-
-    private void AddResponseHeaders(IRestResponse response)
-    {
-        foreach (var header in response.Headers.Where(h =>
-                     _corsBlockedHeaders.Contains(h.Name, StringComparer.InvariantCultureIgnoreCase)))
-            response.Headers.Remove(header);
-
-        foreach (var header in response.Headers.Where(h =>
-                     h.Type == ParameterType.HttpHeader && h.Name != "Transfer-Encoding"))
-            HttpContext.Response.Headers.Add(header.Name, (string)header.Value);
-    }
 
     #endregion
 }
