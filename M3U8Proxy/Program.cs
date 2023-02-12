5b6c2f8b9e2dc9095f79542bcdb2086fe09fdfdf
vynxcc <svynxc@gmail.com>
2023-02-12 18:27:52 -0500

    builder.Services.AddLettuceEncrypt();

--- M3U8Proxy/Program.cs
+++ M3U8Proxy/Program.cs
@@ -4,6 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 var MyAllowSpecificOrigins = "corsPolicy";
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddLettuceEncrypt();
 builder.Services.AddSwaggerGen();
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
