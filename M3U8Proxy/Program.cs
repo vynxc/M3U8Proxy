1492c556dbf0c7e79996d685b8dce3e1d3d827de
vynxcc <svynxc@gmail.com>
2023-02-12 19:50:53 -0500

    fix

--- M3U8Proxy/Program.cs
+++ M3U8Proxy/Program.cs
@@ -1,10 +1,23 @@
+using System.Net;
 using AspNetCore.Proxy;
+using Microsoft.AspNetCore.Server.Kestrel.Https;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 var MyAllowSpecificOrigins = "corsPolicy";
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddLettuceEncrypt();
+builder.WebHost.UseKestrel(k=>{
+    
+    var appServices = k.ApplicationServices;
+    k.Listen(
+        IPAddress.Any, 443,
+        o => o.UseHttps(h =>
+        {
+            h.UseLettuceEncrypt(appServices);
+        }));
+});
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 builder.Services.AddProxies();
@@ -21,7 +34,8 @@ builder.Services.AddCors(options =>
 });
 var app = builder.Build();
 app.UseCors(MyAllowSpecificOrigins);
-app.MapReverseProxy();
+//app.MapReverseProxy();
+
 app.UseSwagger();
 app.UseSwaggerUI();
 app.UseHttpsRedirection();
