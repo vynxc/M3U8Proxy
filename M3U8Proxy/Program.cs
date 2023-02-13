d3e8b20b023d1b098d9e874d830fd8e7c02a9187
vynxcc <svynxc@gmail.com>
2023-02-12 19:09:49 -0500

    testing

--- M3U8Proxy/Program.cs
+++ M3U8Proxy/Program.cs
@@ -1,13 +1,18 @@
 using AspNetCore.Proxy;
+using LettuceEncrypt;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 var MyAllowSpecificOrigins = "corsPolicy";
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddLettuceEncrypt();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddLettuceEncrypt();
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+
+
+
+builder.Services.AddSwaggerGen();
 builder.Services.AddProxies();
 builder.Services.AddCors(options =>
 {
