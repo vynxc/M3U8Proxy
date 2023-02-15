29774e16269d8a287ad97ce5b980c627db57b43f
vynxcc <svynxc@gmail.com>
2023-02-14 22:25:53 -0500

    W

--- M3U8Proxy/Program.cs
+++ M3U8Proxy/Program.cs
@@ -23,9 +23,6 @@ var app = builder.Build();
 app.UseCors(myAllowSpecificOrigins);
 app.UseSwagger();
 app.UseSwaggerUI();
-app.UseHttpsRedirection();
-app.UseAuthentication();
-app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
