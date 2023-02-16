9d966293a6802b9d7af93fba396d61d2b52c13ac
vynxcc <svynxc@gmail.com>
2023-02-16 17:20:18 -0500

    W

--- M3U8Proxy/Program.cs
+++ M3U8Proxy/Program.cs
@@ -9,19 +9,7 @@ const string myAllowSpecificOrigins = "corsPolicy";
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddLettuceEncrypt();
-builder.Services.AddReverseProxy()
-    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 builder.WebHost.UseUrls("https://proxy.vnxservers.com:443");
-builder.WebHost.ConfigureKestrel(kestre =>
-{
-    kestre.ListenAnyIP(5000, listenOptions =>
-    {
-        listenOptions.UseHttps(h =>
-        {
-            h.UseLettuceEncrypt(kestre.ApplicationServices);
-        });
-    });
-});
 
 builder.Services.AddCors(options =>
 {
@@ -38,10 +26,8 @@ var app = builder.Build();
 
 app.UseCors(myAllowSpecificOrigins);
 app.UseSwagger();
-app.MapReverseProxy();
 app.UseSwaggerUI();
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
-
 app.Run();
