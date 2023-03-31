fc88af1e217e7c9ec3a0deb3883be82ceb672b62
chayce <svynxc@gmail.com>
2023-03-31 04:31:46 -0400

    fixed dumb ass allocation

--- M3U8Proxy/RequestHandler/RemoveHeaders.cs
+++ M3U8Proxy/RequestHandler/RemoveHeaders.cs
@@ -4,14 +4,14 @@ namespace M3U8Proxy.RequestHandler;
 
 public partial class ReqHandler
 {
+    private static HashSet<string> _headersToRemove =
+        new(CorsBlockedHeaders.List, StringComparer.InvariantCultureIgnoreCase);
     public static void RemoveBlockedHeaders(IRestResponse response)
     {
-        var headersToRemove =
-            new HashSet<string>(CorsBlockedHeaders.List, StringComparer.InvariantCultureIgnoreCase);
         for (var i = response.Headers.Count - 1; i >= 0; i--)
         {
             var header = response.Headers[i];
-            if (headersToRemove.Contains(header.Name)) response.Headers.RemoveAt(i);
+            if (_headersToRemove.Contains(header.Name)) response.Headers.RemoveAt(i);
         }
     }
 }
