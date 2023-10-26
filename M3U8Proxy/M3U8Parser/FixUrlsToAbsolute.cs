ca340c3225d02b9a84d57f2a569af49aa1d9a17b
chaycee <svynxc@gmail.com>
2023-10-25 23:45:20 -0400

    w

--- M3U8Proxy/M3U8Parser/FixUrlsToAbsolute.cs
+++ M3U8Proxy/M3U8Parser/FixUrlsToAbsolute.cs
@@ -1,11 +1,9 @@
-﻿using System.Text;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 
 namespace M3U8Proxy.M3U8Parser;
 
 public partial class M3U8Paser
 {
-
     [GeneratedRegex(@"\?.+", RegexOptions.Compiled)]
     private static partial Regex GetParamsRegex();
 
@@ -13,18 +11,17 @@ public partial class M3U8Paser
     {
         var parameters = GetParamsRegex().Match(url).Value;
         var uri = new Uri(url);
-        var baseUrl = $"{uri.Scheme}://{uri.Authority}";
-        const string uriPattern =  @"URI=""([^""]+)""";
+        const string uriPattern = @"URI=""([^""]+)""";
         for (var i = 0; i < lines.Length; i++)
         {
             var isUri = lines[i].Contains("URI");
             if (!isUri && (lines[i].StartsWith("#") || string.IsNullOrWhiteSpace(lines[i]))) continue;
-            var uriContent = isUri?Regex.Match(lines[i], uriPattern).Groups[1].Value:lines[i];
+            var uriContent = isUri ? Regex.Match(lines[i], uriPattern).Groups[1].Value : lines[i];
             if (!Uri.TryCreate(uriContent, UriKind.RelativeOrAbsolute, out var uriExtracted)) continue;
             var newUri = !uriExtracted.IsAbsoluteUri ? new Uri(uri, uriExtracted) : uriExtracted;
-            var substitutedUri = $"{prefix}{Uri.EscapeDataString(newUri+parameters)}{suffix}";
+            var substitutedUri = $"{prefix}{Uri.EscapeDataString(newUri + parameters)}{suffix}";
             var test = Regex.Replace(lines[i], uriPattern, m => $"URI=\"{substitutedUri}\"");
-            lines[i] = isUri?test:substitutedUri;
+            lines[i] = isUri ? test : substitutedUri;
         }
 
         return string.Join(Environment.NewLine, lines);
